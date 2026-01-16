afdaa437e41eedc6226e65dfb7eb17f8a940dbea
Maiia <maiyaartemenko@gmail.com>
2026-01-16 05:13:45 +0300

    test html render

--- src/sendpulse/sendpulse-api.js
+++ src/sendpulse/sendpulse-api.js
@@ -46,7 +46,7 @@ export async function sendpulseTelegramSendText({ contactId, text }) {
     },
     body: JSON.stringify({
       contact_id: String(contactId),
-      message: { type: "text", text: String(text), parse_mode: "HTML" }
+      message: { type: "text", text: "<b>ЖИРНЫЙ ТЕСТ</b>", parse_mode: "HTML" }
     })
   });
 
